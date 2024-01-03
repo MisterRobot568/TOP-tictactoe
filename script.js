cea52745b239eb0fc278d3b765bf1b5c374912cb
Austin <christopheraustinwallace@gmail.com>
2024-01-03 05:29:17 -0500

    Finish JS functionality to add pieces to board
    Player can now add piecs to the board, the pieces will be displayed correctly, and the console will determine a winner and stop the game

--- script.js
+++ script.js
@@ -25,6 +25,53 @@ function createPlayer(name, symbol) {
     return { playerName, playerSymbol };
 }
 
+
+//##################################################################
+const displayLogic = (function () {
+    // document.querySelector('.game-container').addEventListener('click', handleSpaceClick)
+    let clickedSpace;
+    // Write a function that will render the contents of the gameboard array to the webpage
+    function renderBoard() {
+        for (let row = 0; row < gameboard.board.length; row++) {
+            for (let col = 0; col < gameboard.board[row].length; col++) {
+                if (gameboard.board[row][col] != '') {
+                    // Write the piece to the board
+                    const space_index = `${row}${col}`;
+                    const board_space = document.querySelector(`[data-index="${space_index}"]`);
+                    board_space.textContent = gameboard.board[row][col];
+
+                }
+            }
+        }
+    }
+    document.querySelector('.game-container').addEventListener('click', handleSpaceClick);
+
+    //write the function that will allow players to add marks to a specific spot on the
+    // board by interacting with the appropriate DOM Elements
+    function handleSpaceClick(playerSymbol, turnTracker) {
+        const currentPlayerSymbol = turnTracker % 2 === 0 ? 'X' : 'O';
+        if (playerSymbol === currentPlayerSymbol) {
+            clickedSpace = playerSymbol.target
+        }
+        // const clickedSpace = playerSymbol.target;
+
+        if (clickedSpace && clickedSpace.classList && clickedSpace.classList.contains("square") && clickedSpace.textContent === '') {
+            const index = clickedSpace.dataset.index;
+            const [row, col] = index.split('');
+            // assuming 'X' and 'O' are player symbols, change accordingly
+            const currentPlayerSymbol = turnTracker % 2 === 0 ? 'X' : 'O';
+
+            //update the game board and render the changes
+            gameboard.add_piece(Number(row), Number(col), currentPlayerSymbol);
+            renderBoard();
+        }
+    }
+
+
+
+    return { renderBoard, handleSpaceClick }
+})();
+//#################################################################
 // 3) object to control the flow of the game, we only need one 
 // instance of this so maybe in a module/ IIFE?
 const gameController = (function () {
@@ -80,30 +127,99 @@ const gameController = (function () {
         return { win, winner }
     }
 
-    while (!game_over) {
-        if (turn_tracker % 2 === 0) {
-            let x_coord = prompt(`${playerOne.playerName} What is your x? `);
-            let y_coord = prompt(`${playerOne.playerName} What is your y? `);
-            gameboard.add_piece(Number(x_coord), Number(y_coord), playerOne.playerSymbol);
-        } else {
-            let x_coord = prompt(`${playerTwo.playerName} What is your x? `);
-            let y_coord = prompt(`${playerTwo.playerName} What is your y? `);
-            gameboard.add_piece(Number(x_coord), Number(y_coord), playerTwo.playerSymbol);
-        }
-        turn_tracker++;
-        // console.log(gameboard.board);
-        // console.log(winChecker().win)
-        if (winChecker().win) {
-            game_over = true;
-            console.log('winner detected')
-            console.log(`${winChecker().winner} wins!`)
-            break;
-        } else if (tieChecker()) {
-            game_over = true;
-            console.log('tie detected')
+    // while (!game_over) {
+    //     if (turn_tracker % 2 === 0) {
+    //         // WE"RE STUCK ON THIS SECTION////////////////////////////////////////////
+    //         //FIRST TRY
+    //         // let x_coord = prompt(`${playerOne.playerName} What is your x? `);
+    //         // let y_coord = prompt(`${playerOne.playerName} What is your y? `);
+    //         // gameboard.add_piece(Number(x_coord), Number(y_coord), playerOne.playerSymbol);
+    //         //SECOND TRY
+    //         // const piece_coords = displayLogic('X')
+    //         // console.log(piece_coords)
+
+    //         // const x_coord = piece_coords[0];
+    //         // const y_coord = piece_coords[1];
+    //         // gameboard.add_piece(Number(x_coord), Number(y_coord), 'X')
+    //         //THIRD TRY
+    //         // const x_move = displayLogic('X');
+    //         // document.querySelector('.game-container').addEventListener('click', x_move.handleSpaceClick)
+    //         displayLogic.handleSpaceClick('X');
+
+
+
+
+    //     } else {
+    //         displayLogic.handleSpaceClick('O');
+    //         // let x_coord = prompt(`${playerTwo.playerName} What is your x? `);
+    //         // let y_coord = prompt(`${playerTwo.playerName} What is your y? `);
+    //         // gameboard.add_piece(Number(x_coord), Number(y_coord), playerTwo.playerSymbol);
+    //         // const piece_coords = displayLogic('O')
+
+    //         // const x_coord = piece_coords[0];
+    //         // const y_coord = piece_coords[1];
+    //         // gameboard.add_piece(Number(x_coord), Number(y_coord), 'O')
+
+    //     }
+    //     turn_tracker++;
+    //     // console.log(gameboard.board);
+    //     // console.log(winChecker().win)
+    //     if (winChecker().win) {
+    //         game_over = true;
+    //         console.log('winner detected')
+    //         console.log(`${winChecker().winner} wins!`)
+    //         break;
+    //     } else if (tieChecker()) {
+    //         game_over = true;
+    //         console.log('tie detected')
+    //     }
+    // }
+
+    // originally we had a while loop to control the flow of the game, 
+    // but we swapped to event listeners because we couldn't get the while
+    // loop to work 
+    function handleGameClick(event) {
+        if (!game_over) {
+            const currentPlayer = turn_tracker % 2 === 0 ? playerOne : playerTwo;
+            const index = event.target.dataset.index;
+
+            if (index && event.target.classList.contains('square') && event.target.textContent === '') {
+                const [row, col] = index.split('');
+                gameboard.add_piece(Number(row), Number(col), currentPlayer.playerSymbol);
+                displayLogic.renderBoard();
+
+                if (winChecker().win) {
+                    game_over = true;
+                    console.log('winner detected')
+                    console.log(`${winChecker().winner} wins!`)
+                } else if (tieChecker()) {
+                    game_over = true;
+                    console.log('tie detected')
+                } else {
+                    turn_tracker++;
+                }
+            }
         }
     }
+    document.querySelector('.game-container').addEventListener('click', handleGameClick);
 
 })();
 
-// STILL NEED TO IMPLEMENT THE LOGIC TO CHECK FOR WINNER/ DRAW
+// Create an object that will handle the DOM/Display logic
+
+
+// function handleSpaceClick(event) {
+//     const clickedSpace = event.target;
+//     if (clickedSpace.classList.contains('square')) {
+//         const index = clickedSpace.dataset.index;
+
+//         if (clickedSpace.textContent === '') {
+//             clickedSpace.textContent = symbol;
+//             return clickedSpace.dataset.index
+//         }
+//     }
+// }
+
+// }
+
+// displayLogic.renderBoard();
